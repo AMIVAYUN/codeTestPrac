220095bacdfd58ca379dbd2985d9c6257e89d92d
AMIVAYUN <wntjrdbs@gmail.com>
2024-01-10 22:58:32 +0900

    feat : BOJ // JAVA 2667 STRING

---
+++ BaekJoon/Java/J2743/Main.java
@@ -0,0 +1,27 @@
+package J2743;
+
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader bf = new BufferedReader( new InputStreamReader( System.in  ) );
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer token;
+    static String[][] patterns = { { "100", "1"}, { "01" } };
+
+
+    public static void main( String[] args ) throws IOException {
+        String row = bf.readLine();
+
+        System.out.println( row.length() );
+
+
+    }
+}
